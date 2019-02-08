be71c9e6626c8022f56492606ba0879878e007ff
Demarbre Allan <demarbreallan@gmail.com>
2019-02-08 07:34:34 +0100

    Syntax ES6 + Adding comments + gitignore + package-lock

--- server.js
+++ server.js
@@ -1,38 +1,59 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 8080;
 
 // Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
+let server = http.createServer((req, res) => {
+
+	console.log(req.url)
+
+	req.url = req.url === '/' ? 'index.html' : req.url;
+
+	fs.readFile(`./${req.url}`, 'utf-8', (error, content) => {
+		res.writeHead(200);
+		res.end(content);
+	});
+
+	/*
+    fs.readFile('./index.html', 'utf-8', (error, content) => {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
-    });
+	});
+	*/
 });
 
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
-var htmlEscape = require('secure-filters').html;
+let io = require('socket.io').listen(server);
+let htmlEscape = require('secure-filters').html;
 
 // Quand un client se connecte, on le note dans la console
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', (socket) => {
 	
-	socket.on('connected', function (data) {
+	socket.on('connected', (data) => {
+
+		console.log(`${data.uname} has joined the chat`);
+
 		data.uname = htmlEscape(data.uname);		
 		data.message = data.uname + ' connected to the chat!';
-   	socket.broadcast.emit('sysmsg', data);
-   	socket.emit('sysmsg', data);
+		socket.broadcast.emit('sysmsg', data);
+		socket.emit('sysmsg', data);
 	});
     
-   socket.on('sendmsg', function (data) {
+  	socket.on('sendmsg', (data) => {
   		data.message = htmlEscape(data.message);    	
     	
 		socket.broadcast.emit('newmsg', data);    
 		socket.emit('newmsg', data);    
-   });
+   	});
     
-   socket.on('disconnect', function (data) {		
+   	socket.on('disconnect', (data) => {		
+		console.log(`Someone has left the chat`);
+		
 		socket.broadcast.emit('sysmsg', {message: 'Somebody left the chat.'});
-   });
+   	});
 });
 
-server.listen(8080);
+server.listen(PORT);
+
+console.log(`Server listening on port ${PORT}`);
